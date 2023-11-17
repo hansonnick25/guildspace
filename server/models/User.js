fb350b841bca66b9ea21e13a5f77811bde317177
steven <smhanson21@gmail.com>
2023-11-16 20:51:31 -0600

    finish User model

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  guilds: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Guild',
+    },
+  ],
+  pals: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema)
+
+module.exports = User
