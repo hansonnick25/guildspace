7d4ff7299e883c2f1d5b972d6fe2a05a95eec82b
Nick Hanson <hansonnick25@gmail.com>
2023-11-30 00:39:41 -0600

    Refactor ApolloClient setup and add MUI theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,108 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token')
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
+const componenets = {
+  MuiButton: {
+    defaultProps: {
+      variant: 'contained',
+    },
+    styleOverrides: {
+      root: {
+        borderRadius: 50,
+        backgroundColor: '#008f11',
+      },
+    },
+  },
+  MuiDrawer: {
+    defaultProps: {
+      variant: 'persistent',
+    },
+    styleOverrides: {
+      paper: {
+        width: 250,
+        flexShrink: 0,
+      },
+    },
+  },
+  MuiCard: {
+    styleOverrides: {
+      root: {
+        alignItems: 'center',
+        margin: 5,
+        borderRadius: 20,
+        backgroundColor: '#003b00',
+      },
+    },
+  },
+  MuiList: {
+    styleOverrides: {
+      root: {
+        backgroundColor: '#',
+        color: 'white',
+      },
+    },
+  },
+  MuiTextField: {
+    defaultProps: {
+      variant: 'filled',
+    },
+  },
+}
+
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      background: {
+        main: '#fff',
+        paper: '#008f11',
+      },
+      primary: {
+        main: '#0d0208',
+        text: '#ffffff',
+      },
+    },
+    components: componenets,
+  })
+
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Outlet />
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Outlet />
+        </div>
+      </ThemeProvider>
     </ApolloProvider>
   )
 }
 
-export default App;
+export default App
